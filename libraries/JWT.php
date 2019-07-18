c2934f9215ea9485f95cba6b955e477a3974b05e
Francis Ilechukwu <francis94c@gmail.com>
2019-07-18 07:01:20 +0100

    Library Boilerplate.

--- libraries/JWT.php
+++ libraries/JWT.php
@@ -2,6 +2,11 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class JWT {
+
+  const JWT = "jwt";
+  
+  // Signing Algorithms.
+  const HS256 = 'HS256';
   function __construct($params=null) {
 
   }
