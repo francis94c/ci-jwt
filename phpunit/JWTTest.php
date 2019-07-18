d80ae360258f7ace2517dce74d444f2ce0094629
Francis Ilechukwu <francis94c@gmail.com>
2019-07-18 07:01:33 +0100

    Header and Payload Test.

--- phpunit/JWTTest.php
+++ phpunit/JWTTest.php
@@ -24,9 +24,46 @@ class JWTTest extends TestCase {
     ];
     self::$ci->load->package(self::PACKAGE, $params);
   }
-
-  function testHeader():void  {
-    
+  /**
+   * [testHelperMethodsExist Test Helper Method Exist.]
+   * @testdox Helper Methods.
+   */
+  function testHelperMethodsExist() {
+    $this->assertTrue(function_exists("base64url_encode"));
+    $this->assertTrue(function_exists("base64url_decode"));
   }
+  /**
+   * [testHeader test header section of JWT]
+   *
+   * @testdox Header Section.
+   */
+  function testHeader():void  {
+    self::$ci->jwt->header("alg", JWT::HS256);
+    self::$ci->jwt->header("typ", JWT::JWT);
+    $header = self::$ci->jwt->headerArray();
+    $this->assertEquals(JWT::HS254, $header["alg"]);
+    $this->assertEquals(JWT::JWT, $header["typ"]);
+  }
+  /**
+   * [testPayload test payload section.]
+   *
+   * RFC7519 Section 4.1
+   *
+   * @testdox Payload Test.
+   */
+  public function testPayload():void  {
+    self::$ci->jwt->payload("iss", "www.example.com");
+    self::$ci->jwt->payload("sub", "francis");
+    self::$ci->jwt->payload("aud", "my_server");
+    self::$ci->jwt->payload("exp", 23456789967);
+    self::$ci->jwt->payload("iat", 12345677778);
+    $payload = self::$ci->jwt->payloadArray();
+    $this->assertEquals("www.example.com", $payload["iss"]);
+    $this->assertEquals("francis", $payload["sub"]);
+    $this->assertEquals("my_server", $payload["aud"]);
+    $this->assertEquals(23456789967, $payload["exp"]);
+    $this->assertEquals(12345677778, $payload["iat"]);
+  }
+
 }
 ?>
