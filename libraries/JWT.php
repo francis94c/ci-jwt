15866000b2d38f7eb6a2f14bf165c1a2448f0d88
Francis Ilechukwu <francis94c@gmail.com>
2020-04-08 03:05:20 +0100

    Dynamically set signing algorithm & magic method __call for payload array access.

--- libraries/JWT.php
+++ libraries/JWT.php
@@ -74,6 +74,7 @@ class JWT
     if ($params != null) $this->init($params);
     get_instance()->load->splint("francis94c/ci-jwt", "%base64");
   }
+
   /**
    * [init For setting config variables.]
    * @param  array  $config Config Options.
@@ -85,22 +86,40 @@ class JWT
     $this->algorithm = $config["algorithm"] ?? $this->algorithm;
     $this->set_iat = $config["set_iat"] ?? $this->set_iat;
   }
+
+  /**
+   * [algorithm description]
+   * @date   2020-04-06
+   * @param  string $algorithm [description]
+   * @return JWT               [description]
+   */
+  public function algorithm(string $algorithm):JWT
+  {
+    $this->algorithm = $algorithm;
+    return $this;
+  }
+
   /**
    * [header Add an item to the header array.]
    * @param  string     $key   Key of the item. e.g "alg", "typ".
    * @param  string|int $value Value of the item.
    */
-  public function header(string $key, $value=null) {
+  public function header(string $key, $value=null)
+  {
     if ($value === null) return $this->header[$key];
     $this->header[$key] = $value;
+    return $this;
   }
+
   /**
    * [headerArray Returns the header array.]
    * @return array Header Array.
    */
-  public function headerArray(): array {
+  public function headerArray(): array
+  {
     return $this->header;
   }
+
   /**
    * [payload Adds an item/claim with a key to the payload array.]
    * @param  string      $key   JWT Claim
@@ -108,35 +127,60 @@ class JWT
    * @return mixed|none         Value of $key if $value == null, else
    *                            returns none[NOTHING].
    */
-  public function payload(string $key, $value=null) {
+  public function payload(string $key, $value=null)
+  {
     if ($value === null) return $this->payload[$key];
     $this->payload[$key] = $value;
+    return $this;
   }
+
+  /**
+   * [__call Magic method, get or set items in the payload array.]
+   * @date   2020-04-06
+   * @param  string     $method Payload field key.
+   * @param  array      $args   Value.
+   * @return [type]             [description]
+   */
+  public function __call(string $method, array $args)
+  {
+    if (count($args) == 0) return $this->payload[$method];
+    $this->payload[$method] = $args[0];
+    return $this;
+  }
+
   /**
    * [iss Convinient function for setting the iss claim]
    * @param  mixed      $iss Value to set the 'iss' claim to.
    * @return mixed|none      Value of the 'iss' claim, if the $iss argument wasn't
    *                         supplied. Otherwise, null.
    */
-  public function iss(string $iss=null) {
+  public function iss(string $iss=null)
+  {
     if ($iss === null) return $this->payload['iss'];
     $this->payload['iss'] = $iss;
+    return $this;
   }
+
   /**
    * [payloadArray Get the payload array.]
    * @return array The payload array.
    */
-  public function payloadArray(): array {
+  public function payloadArray(): array
+  {
     return $this->payload;
   }
+
   /**
    * [create Start afresh, empty/reset header and ]
    * @return [type] [description]
    */
-  public function create():void {
+  public function create():JWT
+  {
     $this->header = [];
     $this->payload = [];
+    return $this;
   }
+
   /**
    * [sign description]
    * @param  [type] $secret [description]
@@ -150,9 +194,9 @@ class JWT
     $this->header["alg"] = $this->header["alg"] ?? ($this->algorithm ?? self::HS512);
     $this->header["typ"] = $this->header["typ"] ?? self::JWT;
     // Generate Issued At Time.
-    if ($this->set_iat) $this->payload["iat"] = $this->payload["iat"] ?? time();
+    if ($this->set_iat) $this->payload["iat"] = $this->payload['iat'] ?? time();
     // Auto Expire.
-    if ($this->auto_expire != null && !isset($this->payload["exp"])) $this->payload["exp"] = strtotime($this->auto_expire);
+    if ($this->auto_expire != null && !isset($this->payload['exp'])) $this->payload['exp'] = strtotime($this->auto_expire);
     $jwt = base64url_encode(json_encode($this->header));
     if ($jwt === false) return null;
     if ($jwt != "") $jwt .= ".";
@@ -161,11 +205,13 @@ class JWT
     if ($key != "") return $this->sign_token($jwt, $key, $this->header["alg"]);
     return $jwt . ".";
   }
+
   /**
    * [token description]
    * @return string [description]
    */
-  public function token():?string {
+  public function token():?string
+  {
     // Checks.
     if  (count($this->payload) == 0) return null;
     // Begin.
@@ -195,6 +241,7 @@ class JWT
     }
     return $this->hashmac($alg, $parts[0] . "." . $parts[1], $parts[2], $key);
   }
+
   /**
    * [expire Sets expiry date of JWT. This basically assigns the return value of
    *         PHP's 'strtotime()' function to the 'exp' field of the payload,
@@ -202,9 +249,11 @@ class JWT
    *         see https://www.php.net/manual/en/function.strtotime.php]
    * @param string $when Future time e.g +1 Week, +1 week 2 days 4 hours 2 seconds.
    */
-  public function expire(string $when):void {
+  public function expire(string $when):void
+  {
     $this->payload["exp"] = strtotime($when);
   }
+
   /**
    * [decode description]
    * @param  string  $jwt [description]
@@ -226,9 +275,10 @@ class JWT
    * @return bool        [description]
    */
   public function expired(string $jwt=null):bool {
-    $exp = $jwt == null ? ($this->payload["exp"] ?? time()) : $this->get_expired($jwt);
+    $exp = $jwt == null ? ($this->payload["exp"] ?? time() + 4) : $this->get_expired($jwt);
     return time() >= $exp;
   }
+
   /**
    * [hashmac description]
    * @param  string $alg       [description]
@@ -240,15 +290,18 @@ class JWT
   private function hashmac(string $alg, string $data, string $signature, string $secret):bool {
     return $signature === hash_hmac(self::ALGOS[$alg], $data, $secret);
   }
+
   /**
    * [get_expired description]
    * @param  string $jwt [description]
    * @return int         [description]
    */
-  private function get_expired(string $jwt):int {
+  private function get_expired(string $jwt):int
+  {
     $parts = explode(".", $jwt);
-    return json_decode(base64url_decode($parts[1]) ,true)["exp"] ?? time();
+    return json_decode(base64url_decode($parts[1]) ,true)["exp"] ?? time() + 4;
   }
+
   /**
    * [sign_token Sign JWT]
    * @param  string $token base64 url encoded header and payload token pair.
@@ -256,7 +309,8 @@ class JWT
    * @param  string $alg   The algorithm used to sign the token.
    * @return string        Complete JWT.
    */
-  private function sign_token(string $token, string $key, string $alg):string {
+  private function sign_token(string $token, string $key, string $alg):string
+  {
     if ($alg == self::NONE) return $token . ".";
     $token = rtrim($token, ".");
     $signature = hash_hmac(self::ALGOS[$alg], $token, $key);
